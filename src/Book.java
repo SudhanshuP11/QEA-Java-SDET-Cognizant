47da3b5f42615f259f518cfb09958f073a5b8d61
Sudhanshu Pathak <connect.sudhanshupathak@gmail.com>
2026-02-17 11:40:04 +0530

    Added Book

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Book {
+    String title;
+    String author;
+    double price;
+
+    void displayDetails() {
+        System.out.println("Title: " + title+ " Author: " + author + " Price: " + price);
+
+    }
+
+
+}
+class Main {
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.title = "Java basic";
+        book.author = "Oracle";
+        book.price = 25.5;
+        book.displayDetails();
+
+        Book book2 = new Book();
+        book2.title = "Clean Code";
+        book2.author = "Robert Martin";
+        book2.price = 45.00;
+        book2.displayDetails();
+
+    }
 }
