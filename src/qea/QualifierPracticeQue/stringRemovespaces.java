6652c5e8832efc79fee1f8725a6d70eb2e51c39d
Sudhanshu Pathak <connect.sudhanshupathak@gmail.com>
2026-03-30 14:49:53 +0530

    Initial

---
+++ src/qea/QualifierPracticeQue/stringRemovespaces.java
@@ -0,0 +1,12 @@
+package qea.QualifierPracticeQue;
+
+import java.util.Scanner;
+
+public class stringRemovespaces {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = sc.nextLine();
+        System.out.println(s.replace(" ",""));
+    }
+}
