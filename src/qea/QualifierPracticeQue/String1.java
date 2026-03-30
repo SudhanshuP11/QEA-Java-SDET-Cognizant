3e215b017ee1ea2b056d6949de6523e956e6dd6e
Sudhanshu Pathak <connect.sudhanshupathak@gmail.com>
2026-03-30 14:20:42 +0530

    Initial

---
+++ src/qea/QualifierPracticeQue/String1.java
@@ -0,0 +1,19 @@
+package qea.QualifierPracticeQue;
+
+import java.util.Scanner;
+
+// count length of the string withou using length
+public class String1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String a = sc.nextLine();
+        System.out.println(a.length());
+        char[] b = a.toCharArray();
+        int count =0;
+        for(char c :b){
+            count++;
+        }
+        System.out.print(count);
+    }
+}
