944eb467df417f8050e3035a4b5f442267e605d5
Sudhanshu Pathak <connect.sudhanshupathak@gmail.com>
2026-03-30 16:47:44 +0530

    Initial

---
+++ src/qea/QualifierPracticeQue/Arrays/arrayRevrDesc.java
@@ -0,0 +1,27 @@
+package qea.QualifierPracticeQue.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class arrayRevrDesc {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n =  sc.nextInt();
+        int [] a = new int[n];
+        for (int i=0;i<n;i++){
+            System.out.println("Enter the "+(i+1)+" element: ");
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        int left=0, right = a.length-1;
+        while(left<right){
+            int temp=a[left];
+            a[left]=a[right];
+            a[right]=temp;
+            left++;
+            right--;
+        }
+        System.out.print(Arrays.toString(a));
+    }
+}
